d7fc2e1b492f75ccd5cc7ed200a10a639b2931f2
Mark Wetzel <mark@markwetzel.com>
2014-03-27 10:55:54 -0400

    Fixed bug with user profile validation.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -39,4 +39,12 @@ class UserTest < ActiveSupport::TestCase
   	assert !user.errors[:profile_name].empty?
   	assert user.errors[:profile_name].include?("Must be formatted correctly.")
   end
+
+  test "a user should have a correctly formatted profile name" do
+  	user = users(:mark)
+  	user.password = user.password_confirmation = "password"
+  	user.profile_name = "markwetzel_1"
+
+  	assert user.valid?
+  end
 end
